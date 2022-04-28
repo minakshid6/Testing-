cf8abeb073ca1eda27510732c8222419d7478e5d
minakshid6 <104327664+minakshid6@users.noreply.github.com>
2022-04-27 23:57:39 -0500

    done

--- Java gitjenkin project/src/CarName.java
+++ Java gitjenkin project/src/CarName.java
@@ -8,6 +8,7 @@ public class CarName {
 	System.out.println("this is Tesla electric car");
 	System.out.println("this is volvo xc90 clour black");
 	System.out.println("this is Accura mdx");
+	System.out.println("*****done********");
 	
 	
 	

cb51781e4f2930fd34a431a5b503b39db24f97e1
minakshid6 <104327664+minakshid6@users.noreply.github.com>
2022-04-27 23:24:11 -0500

    car project by minakshi

---
+++ Java gitjenkin project/src/CarName.java
@@ -0,0 +1,13 @@
+
+public class CarName {
+
+	public static void main(String[] args) {
+		
+	System.out.println("this is Audi colour moonlight blue ");
+	System.out.println("this is BMW CLOUR black");
+	System.out.println("this is Tesla electric car");
+	System.out.println("this is volvo xc90 clour black");
+	
+	}
+
+}
